a1f55e758d4e5dc20d2f74adaf279c1475595144
jmjwlj <jmjwlj@gmail.com>
2021-06-22 21:54:21 +0800

    更新

--- code/webpackGenerate/lib/compiler.js
+++ code/webpackGenerate/lib/compiler.js
@@ -17,19 +17,57 @@ class compiler{
         const depsGraph = this.modules.reduce((graph, module) => {
           return {
               ...graph,
-              [module.entryPath]:{
+              [module.absolutePath]:{
                   code: module.code,
                   deps: module.deps
               }
           }
         }, {})
+        this.generate(depsGraph)
+    }
+
+    // 递归获取依赖
+    getModules(absolutePath) {
+        const module = this.build(absolutePath)
+        const { deps } = module
+        this.modules.push(module)
+        for(let relativePath in deps) {
+            this.getModules(deps[relativePath])
+        }
+    }
+
+    // 获取单模块信息
+    build(absolutePath) {
+        // 获取babelParser编译的ast
+        // sourceType:module
+        // es module类型的ast语法树
+        const ast = getAst(absolutePath)
+        // 获取依赖deps
+        /*"deps": {
+            "./add.js": "C:\\Users\\mj\\Desktop\\webpack5\\code\\webpackGenerate\\src\\add.js",
+            "./count.js": "C:\\Users\\mj\\Desktop\\webpack5\\code\\webpackGenerate\\src\\count.js"
+        }*/
+        const deps = getDeps(ast, absolutePath)
+        // 获取入口code
+        const code = getCode(ast)
+        return {
+            absolutePath, // 绝对路径
+            deps, // 依赖对象
+            code // 经过babel转换字符串code
+        }
+    }
+
+    // 生成代码文件
+    generate(depsGraph) {
         /*
         depsGraph结构表
           {
             "C:\Users\mj\Desktop\webpack5\code\webpackGenerate\src\index.js": {
                 "code": "\"use strict\";\n
+                    // require函数其实是loaderRequire函数,其返回值是runCode中exports私有变量
                     \n var _add = _interopRequireDefault(require(\"./add.js\"));
                     \n\n var _count = _interopRequireDefault(require(\"./count.js\"));
+                    // 判断exports是否为esModule,不是则创建对应对象
                     \n\n function _interopRequireDefault(obj) {
                         return obj && obj.__esModule ? obj : { \"default\": obj };
                     }
@@ -42,6 +80,7 @@ class compiler{
            },
             "C:\\Users\\mj\\Desktop\\webpack5\\code\\webpackGenerate\\src\\add.js": {
                 "code": "\"use strict\";\n
+                    // 装载私有的exports变量,为babel类型的exports,然后续代码可用
                     \n Object.defineProperty(exports, \"__esModule\", {\n  value: true\n});
                     \n exports[\"default\"] = add;
                     \n\n function add(a, b) {\n  return a + b;\n}",
@@ -49,6 +88,7 @@ class compiler{
            },
             "C:\\Users\\mj\\Desktop\\webpack5\\code\\webpackGenerate\\src\\count.js": {
                 "code": "\"use strict\";\n
+                    // 装载私有的exports变量,为babel类型的exports,然后续代码可用
                     \n Object.defineProperty(exports, \"__esModule\", {\n  value: true\n});
                     \n exports[\"default\"] = count;
                     \n\n function count(a, b) {\n  return a - b;\n}",
@@ -56,42 +96,6 @@ class compiler{
             }
          }
         */
-        this.generate(depsGraph)
-    }
-
-    // 递归获取依赖
-    getModules(entryPath) {
-        const module = this.build(entryPath)
-        const { deps } = module
-        this.modules.push(module)
-        for(let relativePath in deps) {
-            this.getModules(deps[relativePath])
-        }
-    }
-
-    // 获取单模块信息
-    build(entryPath) {
-        // 获取babelParser编译的ast
-        // sourceType:module
-        // es module类型的ast语法树
-        const ast = getAst(entryPath)
-        // 获取依赖deps
-        /*"deps": {
-            "./add.js": "C:\\Users\\mj\\Desktop\\webpack5\\code\\webpackGenerate\\src\\add.js",
-            "./count.js": "C:\\Users\\mj\\Desktop\\webpack5\\code\\webpackGenerate\\src\\count.js"
-        }*/
-        const deps = getDeps(ast, entryPath)
-        // 获取入口code
-        const code = getCode(ast)
-        return {
-            entryPath, // 绝对路径
-            deps, // 依赖对象
-            code // 经过babel转换字符串code
-        }
-    }
-
-    // 生成代码文件
-    generate(depsGraph) {
         const entryPath = path.resolve(process.cwd(),this.options.entry)
         const bundle = `(function(depsGraph) {
             function runCode(absolutePath){
